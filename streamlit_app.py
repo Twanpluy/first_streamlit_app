23b127e1de66830e4371e2f3cba9a283dc02364f
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-22 00:21:25 +0200

    removed .txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # # new section to display the fruityvice api response
-# fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + 'kiwi')
-# # normelize data view
-# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# streamlit.dataframe(fruityvice_normalized)
-
 # new section with input fields
 streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input('Enter your fruit of choice: ', 'kiwi')
