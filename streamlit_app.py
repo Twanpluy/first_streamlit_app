c2fc9978bffc1c53a7cf933d93432e5fb4cb9692
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-21 18:11:57 +0200

    added list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+
+# lets put the fruit list in a streamlit selectbox
+streamlit.multiselect('Select your fruits: ',list(my_fruit_list.index))
+
+#display items in the list
 streamlit.dataframe(my_fruit_list)
