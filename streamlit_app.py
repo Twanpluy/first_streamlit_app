ee0a06ff9d0b51d5f4e60d04648a2cb53ce6e646
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-21 22:41:57 +0200

    Added a api call with input

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,10 @@ fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + 'kiwi')
 # normelize data view
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+# new section with input fields
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('Enter your fruit of choice: ', 'kiwi')
+streamlit.write('the user enterd:', fruit_choice)
+
+fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
