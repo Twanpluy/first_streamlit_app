140c70d38424ca4a35dd502a2bc6f585e8bf5abd
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-21 17:56:09 +0200

    update with breakfast menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
-streamlit.text('test')
+streamlit.headder('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry OatMeal')
+streamlit.text('Kale, Spinach & Broccoli Smoothie')
+streamlit.text('Hard-Boiled Eggs')
