7360b1d53ed770738e836956501a490dad1d7acc
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-21 17:56:52 +0200

    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
-streamlit.headder('Breakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 & Blueberry OatMeal')
 streamlit.text('Kale, Spinach & Broccoli Smoothie')
 streamlit.text('Hard-Boiled Eggs')
