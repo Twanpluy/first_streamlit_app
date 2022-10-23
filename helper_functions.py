aa743f6f7478bccba99dc2f4d8fb526590b3dcf7
Twan Pluymaekers <twanpluy@gmail.com>
2022-10-23 13:10:13 +0200

    typo fix

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-### function to gget fruityvuice data ###
+### function to get fruityvuice data ###
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{this_fruit_choice}')
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
